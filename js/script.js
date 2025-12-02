6e46e56361a2c643dea1205e9f4ba3030faf847d
OlaReda <ola.reda.900@gmail.com>
2025-12-02 16:47:44 +0200

    Add vailition

--- js/script.js
+++ js/script.js
@@ -1,23 +1,71 @@
 var inputName = document.querySelector("#InputName")
 var inputUrl = document.querySelector("#InputUrl")
-console.log(inputName, inputUrl)
-
 
 var list = [];
+if(localStorage.getItem("data")) {
+    list = JSON.parse(localStorage.getItem("data"))
+    displayData()
+}
+
+
+function nameValidation() {
+    var regax = /^[a-zA-Z0-9]{3,15}$/
+    var myName = inputName.value
+    regax.test(myName)
+    if(regax.test(myName)){
+        inputName.classList.add("is-valid")
+        inputName.classList.remove("is-invalid")
+        return true
+    }
+    else {
+        inputName.classList.add("is-invalid")
+        inputName.classList.remove("is-valid")
+        return false
+    }
+}
+
+
+function urlValidation() {
+    var regax = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+    var myUrl = inputUrl.value
+    regax.test(myUrl)
+    if(regax.test(myUrl)) {
+        inputUrl.classList.add("is-valid")
+        inputUrl.classList.remove("is-invalid")
+        return true
+    }
+    else {
+        inputUrl.classList.add("is-invalid")
+        inputUrl.classList.remove("is-valid")
+        return false
+    } 
+}
+
+
+
 //function to add data
 function addData() {
-    var data = {
+    if(nameValidation() && urlValidation()){
+        var data = {
         name: inputName.value,
         url: inputUrl.value,
     }
     list.push(data)
-    displayDta()
+    localStorage.setItem("data",JSON.stringify(list))
+    displayData()
     clearInputsValues();
+    }else {
+        Swal.fire({
+            title: "The Internet?",
+            text: "That thing is still around?",
+            icon: "question"
+            });
+        }
 }
 
 
 //function to display data
-function displayDta() {
+function displayData() {
     var box = document.querySelector(".content")
     var contentBox = ''
     for (var i = 0; i < list.length; i++) {
@@ -36,7 +84,8 @@ function displayDta() {
 //function to delete data
 function deleteData(index) {
     list.splice(index, 1)
-    displayDta()
+    localStorage.setItem("data",JSON.stringify(list))
+    displayData()
 }
 
 function clearInputsValues() {
